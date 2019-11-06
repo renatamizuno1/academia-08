060a5908e71f98b77630c3cab7ff5a367c31165d
Gabriel Alves <renata.mizuno.25@gmail.com>
2019-11-05 21:59:17 -0300

    javascript

---
+++ aula-07/script.js
@@ -0,0 +1,22 @@
+const nome = "renata";
+const s = "mizuno";
+
+var login = prompt("nome de usuario");
+var senha = prompt("digite sua senha");
+
+if(nome == login && s == senha){ 
+  alert(" vc esta logado")
+}
+
+else{
+    alert("INVALIDO")
+}
+
+while(const switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+})
