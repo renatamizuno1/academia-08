15b189d8534132d8b3f7eb61a53c4868eb10db27
unknown <renata.mizuno.25@gmail.com>
2019-11-07 19:29:04 -0300

    aula-08

---
+++ aula-09/script.js
@@ -0,0 +1,8 @@
+let material = prompt("qual material você quer?")
+let qtDegraus = Number(prompt("quantos degraus você que?"))
+let escada = ""
+
+for(let i = 0; i < qtDegraus; i++){
+escada = material + material
+console.log(escada)
+}
