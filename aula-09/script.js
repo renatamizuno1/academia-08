3a07272c4c0989b3dac8ba6dc346ba2c93f893a1
unknown <renata.mizuno.25@gmail.com>
2019-11-07 21:54:18 -0300

    primeiro commit

--- aula-09/script.js
+++ aula-09/script.js
@@ -1,8 +1,44 @@
-let material = prompt("qual material você quer?")
-let qtDegraus = Number(prompt("quantos degraus você que?"))
-let escada = ""
+// // function funcao (parametro1, parametro2) {
+// //     // executar aq dentro
+// //     return
+// // }
 
-for(let i = 0; i < qtDegraus; i++){
-escada = material + material
-console.log(escada)
-}
+// // console.log(funcao(argumento1, argumento2));
+
+// // // variavel receber uma função
+
+// // let a = function hoho(){
+// //     return "hoho"
+// // }
+
+// // //  função que recebe argumentos na declaração
+
+// // function somar(){
+// //     let soma = 0;
+// //     for (let i =0 )
+// // }
+
+
+// // arrow functions
+
+// let x = (param1, param2) => {
+//     conteudo da minha função
+// }
+
+// let lerolero = ["bla bla bla", "mimimi", "hahaha"]
+
+
+
+
+
+
+// var x = document.querySelectorAll('section');
+// x[2]
+// section id="contato">...</section>
+
+// var x = document.querySelectorAll('h3');
+// x[2].innerHTML = 'kkkkkkkk mas eu to rindo atoa'
+
+// var y = document.querySelectorAll('cabecalho')
+
+// document.querySelectorAll('#cabecalho').style.backgroundColor = 'black'
---
+++ aula-09/scriptBotao.js
@@ -0,0 +1,15 @@
+var botao = document.querySelector("button");
+
+function mudarBotao(){
+    botao.style.background = blue;
+}
+
+botao.onclick = mudarBotao;
+
+let botao = document.querySelector("button");
+
+function tirarClass(){
+ // if(botao.classList.contains("humilde")){
+//    botao.classList.remove("humilde");
+// } else {
+//  botao.classList.add("humilde")
