32eecc5a45081fc034db92922412aaa25ac04e6c
unknown <renata.mizuno.25@gmail.com>
2019-11-14 21:31:25 -0300

    primeiro commit

---
+++ aula-14/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\renomear.desktop-bs0u13s\desktop\academia-08\aula-14\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
